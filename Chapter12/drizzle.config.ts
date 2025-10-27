a6257af0d5c442dcbf1a535208c4f94121c6c41c
FoggDev <foggdev@gmail.com>
2025-10-26 17:13:48 -0700

    Fixing Chapter 12

--- Chapter12/drizzle.config.ts
+++ Chapter12/drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'drizzle-kit';
-import { config } from './src/config.js';
+import type { Config } from "drizzle-kit";
+import { config } from "./src/config";
 
 export default {
-  schema: './src/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
-    url: config.DATABASE_URL
-  }
+    url: "postgresql://postgres:12345678@localhost:5432/chapter12",
+  },
 } satisfies Config;
